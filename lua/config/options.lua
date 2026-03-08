4a63be19064d8446b4446329084ffe56b4f73d04
Eugenio Luo <93342423+eugenio-luo@users.noreply.github.com>
2026-03-08 20:16:16 +0000

    Add telescope plugin and its shortcut mappings

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -10,3 +10,9 @@ vim.g.autoformat = false
 vim.diagnostic.config({ virtual_text = false })
 
 vim.g.mapleader = ","
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>df', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>dg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>db', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>dh', builtin.help_tags, { desc = 'Telescope help tags' })
